3befa3410c1a64e14fc43d4c4a5dfff91f712254
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-09-16 16:31:52 +0200

    Fixed typo on calculator, add ac, del and %, refactor code

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 let lastInput;
+let input;
+
 let result;
+
+// first user inputs one number
+    // input is displayed when chosen
+
+// second user inputs a operator 
+    // operator is displayed when chosen
+
+// third the result is printed to the DOM
+    // when the user want to calculate another calculation, display the last calculation
+
+// user should also be able to delete current input
+// user should also be able to delete one number from calculation
+
 
